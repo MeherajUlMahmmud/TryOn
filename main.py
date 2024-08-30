0fbebab08a37023aeefec8251807dc56d0bd7ad2
Meheraj <57069206+MeherajUlMahmmud@users.noreply.github.com>
2024-08-30 23:40:47 +0600

    updated

--- main.py
+++ main.py
@@ -14,48 +14,54 @@ T_SHIRT_PATH = Constant.t_shirt_path
 
 logger = setup_logger()
 
+logger.info("====================================================")
+logger.info("Starting up...")
+
 selected_t_shirt: Union[np.ndarray, Any] = None
-t_shirts: List[str] = os.listdir(T_SHIRT_PATH)
+t_shirt_paths: List[str] = os.listdir(T_SHIRT_PATH)
 loaded_t_shirts: List[np.ndarray] = []
 selected_t_shirt_idx: Union[int, Any] = None  # To track the selected T-shirt
-
-cap = cv2.VideoCapture(0)
-if not cap.isOpened():
-    logger.error("Camera not opened")
-    logger.info("Exiting...")
-    exit()
-
-logger.info("Camera opened")
-
 frame_shape = (0, 0)
-detector = PoseDetector()
-run: bool = True
-
-logger.info("Loading t-shirt images")
-# Load all T-shirt images
-for t_shirt in t_shirts:
-    t_shirt_image: np.ndarray = cv2.imread(os.path.join(T_SHIRT_PATH, t_shirt))
-
-    # Check if the image is loaded correctly
-    if t_shirt_image is None:
-        logger.error(f"Failed to load {t_shirt}. Skipping this file.")
-        continue
-
-    loaded_t_shirts.append(t_shirt_image)
-logger.info(f"Loaded {len(loaded_t_shirts)} t-shirts images")
 
 
-def show_t_shirt_list(frame: np.ndarray, t_shirts: List[np.ndarray], selected_idx: int) -> np.ndarray:
+def load_t_shirt_images():
+    global loaded_t_shirts
+
+    # Load all T-shirt images
+    logger.info("Loading t-shirt images")
+    for t_shirt in t_shirt_paths:
+        t_shirt_image: np.ndarray = cv2.imread(
+            os.path.join(T_SHIRT_PATH, t_shirt), cv2.IMREAD_UNCHANGED)
+
+        # Check if the image is loaded correctly
+        if t_shirt_image is None:
+            logger.error(f"Failed to load {t_shirt}. Skipping this file.")
+            continue
+
+        loaded_t_shirts.append(t_shirt_image)
+    logger.info(f"Loaded {len(loaded_t_shirts)} t-shirts images")
+
+
+def show_t_shirt_list(
+        frame: np.ndarray,
+        t_shirts: List[np.ndarray],
+        selected_idx: int,
+        position: str = "bottom-center"
+) -> np.ndarray:
     """
-    Displays a list of T-shirt icons at the bottom of the given frame, highlighting the selected one.
+    Displays a list of T-shirt icons at the specified position on the frame, highlighting the selected one.
 
     Args:
         frame (np.ndarray): The frame on which to display the T-shirt icons.
         t_shirts (List[np.ndarray]): A list of T-shirt images to display as icons.
         selected_idx (int): The index of the currently selected T-shirt.
+        position (str): The position on the screen where the icons should be displayed.
+                        Options: "top-left", "top-center", "top-right",
+                                 "bottom-left", "bottom-center", "bottom-right",
+                                 "center-left", "center-center", "center-right".
 
     Returns:
-        np.ndarray: The frame with T-shirt icons displayed at the bottom.
+        np.ndarray: The frame with T-shirt icons displayed at the specified position.
     """
     icon_size = 50  # Size of each T-shirt icon (width and height)
     spacing = 10  # Space between each icon
@@ -69,9 +75,30 @@ def show_t_shirt_list(frame: np.ndarray, t_shirts: List[np.ndarray], selected_id
     # Calculate y offset to position the icons near the bottom of the frame
     y_offset = frame_height - icon_size - spacing
 
-    for idx, t_shirt in enumerate(t_shirts):
+    # # Calculate x offset based on the position
+    # if "left" in position:
+    #     x_offset = spacing
+    # elif "center" in position:
+    #     x_offset = (frame_width - total_width) // 2
+    # else:  # "right"
+    #     x_offset = frame_width - total_width - spacing
+    #
+    # # Calculate y offset based on the position
+    # if "top" in position:
+    #     y_offset = spacing
+    # elif "center" in position:
+    #     y_offset = (frame_height - icon_size) // 2
+    # else:  # "bottom"
+    #     y_offset = frame_height - icon_size - spacing
+
+    for idx, t_shirt_img in enumerate(t_shirts):
         # Resize the T-shirt image to the icon size
-        t_shirt_icon = cv2.resize(t_shirt, (icon_size, icon_size))
+        t_shirt_icon = cv2.resize(t_shirt_img, (icon_size, icon_size))
+
+        # Check if the resized image has 4 channels (RGBA)
+        if t_shirt_icon.shape[2] == 4:
+            # Convert RGBA to RGB by removing the alpha channel
+            t_shirt_icon = cv2.cvtColor(t_shirt_icon, cv2.COLOR_RGBA2RGB)
 
         # Place the resized icon onto the frame at the calculated position
         frame[y_offset:y_offset + icon_size, x_offset:x_offset + icon_size] = t_shirt_icon
@@ -93,7 +120,7 @@ def show_t_shirt_list(frame: np.ndarray, t_shirts: List[np.ndarray], selected_id
     return frame
 
 
-def click_event(event, x, y, flags, param):
+def click_event(event: int, x: int, y: int, flags, param):
     """
     Handles mouse click events to select or deselect a T-shirt icon displayed on the frame.
 
@@ -135,20 +162,20 @@ def click_event(event, x, y, flags, param):
                 break
 
 
-def get_landmark(landmarks_list: List[Tuple[int, int, int]], idx):
+def get_landmark(landmarks: List[Tuple[int, int, int]], idx):
     """
     Retrieves the x and y coordinates of a specific landmark from the landmarks list.
 
     Args:
-        landmarks_list (List[List[int]]): A list of landmarks, where each landmark is a list containing x and y coordinates.
+        landmarks (List[List[int]]): A list of landmarks, where each landmark is a list containing x and y coordinates.
         idx (int): The index of the landmark to retrieve.
 
     Returns:
         tuple: A tuple (x, y) representing the coordinates of the landmark if the index is valid.
         None: If the index is out of range or the landmarks list is empty.
     """
-    if 0 <= idx < len(landmarks_list):
-        return landmarks_list[idx][0], landmarks_list[idx][1]
+    if 0 <= idx < len(landmarks):
+        return landmarks[idx][0], landmarks[idx][1], landmarks[idx][2]
     return None
 
 
@@ -156,126 +183,143 @@ def get_landmark(landmarks_list: List[Tuple[int, int, int]], idx):
 cv2.namedWindow(APP_NAME)
 cv2.setMouseCallback(APP_NAME, click_event)
 
-# Main loop
-while run:
-    ret, frame = cap.read()
-    frame_shape = frame.shape
-    frame = detector.find_pose(frame)
-    landmarks_list, bbox_info = detector.find_position(
-        frame,
-        bbox_with_hands=False,
-    )
 
-    if bbox_info:
-        center = bbox_info["center"]
-        cv2.circle(frame, center, 5, (255, 0, 255), cv2.FILLED)
+def main():
+    global frame_shape
 
-    # Display the T-shirt icons on the frame
-    frame = show_t_shirt_list(frame, loaded_t_shirts, selected_t_shirt_idx)
+    cap = cv2.VideoCapture(0)
+    if not cap.isOpened():
+        logger.error("Camera not opened")
+        logger.info("Exiting...")
+        exit()
+    logger.info("Camera opened")
 
-    # Get post data from landmarks
-    left_shoulder = get_landmark(landmarks_list, 12)
-    right_shoulder = get_landmark(landmarks_list, 13)
-    left_elbow = get_landmark(landmarks_list, 14)
-    right_elbow = get_landmark(landmarks_list, 15)
-    left_hip = get_landmark(landmarks_list, 24)
-    right_hip = get_landmark(landmarks_list, 25)
+    load_t_shirt_images()
 
-    print("Left Shoulder: ", left_shoulder)
-    print("Right Shoulder: ", right_shoulder)
-    print("Left Elbow: ", left_elbow)
-    print("Right Elbow: ", right_elbow)
-    print("Left Hip: ", left_hip)
-    print("Right Hip: ", right_hip)
+    detector = PoseDetector()
+    run: bool = True
 
-    if left_shoulder:
-        left_shoulder_x, left_shoulder_y = left_shoulder[0], left_shoulder[1]
-        Helper.put_text_on_frame(
+    # Main loop
+    while run:
+        success, frame = cap.read()
+        if not success:
+            break
+
+        frame_shape = frame.shape
+        frame = detector.find_pose(frame)
+        landmarks_list, bbox_info = detector.find_position(
             frame,
-            f"Left Shoulder: ({left_shoulder_x}, {left_shoulder_y})",
-            (left_shoulder_x, left_shoulder_y - 20),
+            bbox_with_hands=False,
         )
 
-    if right_shoulder:
-        right_shoulder_x, right_shoulder_y = right_shoulder[0], right_shoulder[1]
-        Helper.put_text_on_frame(
-            frame,
-            f"Right Shoulder: ({right_shoulder_x}, {right_shoulder_y})",
-            (right_shoulder_x, right_shoulder_y - 20),
-        )
+        if bbox_info:
+            center = bbox_info["center"]
+            cv2.circle(frame, center, 5, (255, 0, 255), cv2.FILLED)
 
-    if left_elbow:
-        left_elbow_x, left_elbow_y = left_elbow[0], left_elbow[1]
-        Helper.put_text_on_frame(
-            frame,
-            f"Left Elbow: ({left_elbow_x}, {left_elbow_y})",
-            (left_elbow_x, left_elbow_y - 20),
-        )
+        # Display the T-shirt icons on the frame
+        frame = show_t_shirt_list(frame, loaded_t_shirts, selected_t_shirt_idx)
 
-    if right_elbow:
-        right_elbow_x, right_elbow_y = right_elbow[0], right_elbow[1]
-        Helper.put_text_on_frame(
-            frame,
-            f"Right Elbow: ({right_elbow_x}, {right_elbow_y})",
-            (right_elbow_x, right_elbow_y - 20),
-        )
+        # Get post data from landmarks
+        left_shoulder = get_landmark(landmarks_list, 11)
+        right_shoulder = get_landmark(landmarks_list, 12)
+        left_elbow = get_landmark(landmarks_list, 13)
+        right_elbow = get_landmark(landmarks_list, 14)
+        left_hip = get_landmark(landmarks_list, 23)
+        right_hip = get_landmark(landmarks_list, 24)
 
-    if left_hip:
-        left_hip_x, left_hip_y = left_hip[0], left_hip[1]
-        Helper.put_text_on_frame(
-            frame,
-            f"Left Hip: ({left_hip_x}, {left_hip_y})",
-            (left_hip_x, left_hip_y - 20),
-        )
+        # print("Left Shoulder: ", left_shoulder)
+        # print("Right Shoulder: ", right_shoulder)
+        # print("Left Elbow: ", left_elbow)
+        # print("Right Elbow: ", right_elbow)
+        # print("Left Hip: ", left_hip)
+        # print("Right Hip: ", right_hip)
 
-    if right_hip:
-        right_hip_x, right_hip_y = right_hip[0], right_hip[1]
-        Helper.put_text_on_frame(
-            frame,
-            f"Right Hip: ({right_hip_x}, {right_hip_y})",
-            (right_hip_x, right_hip_y - 20),
-        )
+        if left_shoulder:
+            left_shoulder_x, left_shoulder_y = left_shoulder[0], left_shoulder[1]
+            Helper.put_text_on_frame(
+                frame,
+                f"Left Shoulder: ({left_shoulder_x}, {left_shoulder_y})",
+                (left_shoulder_x, left_shoulder_y - 20),
+            )
 
-    if selected_t_shirt is not None:
-        # Determine where you want to place the selected T-shirt on the frame
-        shirt_height, shirt_width = selected_t_shirt.shape[:2]
+        if right_shoulder:
+            right_shoulder_x, right_shoulder_y = right_shoulder[0], right_shoulder[1]
+            Helper.put_text_on_frame(
+                frame,
+                f"Right Shoulder: ({right_shoulder_x}, {right_shoulder_y})",
+                (right_shoulder_x, right_shoulder_y - 20),
+            )
 
-        if left_shoulder and right_shoulder:
-            shoulder_width = right_shoulder[0] - left_shoulder[0]
+        if left_elbow:
+            left_elbow_x, left_elbow_y = left_elbow[0], left_elbow[1]
+            Helper.put_text_on_frame(
+                frame,
+                f"Left Elbow: ({left_elbow_x}, {left_elbow_y})",
+                (left_elbow_x, left_elbow_y - 20),
+            )
 
-            if left_hip and right_hip:
-                hip_width = right_hip[0] - left_hip[0]
-                # Resize the T-shirt to fit between the shoulders and hips
-                width = min(shoulder_width, hip_width)
-                # Maintain aspect ratio
-                height = int((width / shirt_width) * shirt_height)
+        if right_elbow:
+            right_elbow_x, right_elbow_y = right_elbow[0], right_elbow[1]
+            Helper.put_text_on_frame(
+                frame,
+                f"Right Elbow: ({right_elbow_x}, {right_elbow_y})",
+                (right_elbow_x, right_elbow_y - 20),
+            )
 
-                # Resize the T-shirt image
-                resized_t_shirt = cv2.resize(selected_t_shirt, (width, height))
+        if left_hip:
+            left_hip_x, left_hip_y = left_hip[0], left_hip[1]
+            Helper.put_text_on_frame(
+                frame,
+                f"Left Hip: ({left_hip_x}, {left_hip_y})",
+                (left_hip_x, left_hip_y - 20),
+            )
 
-                # Position the T-shirt between the shoulders and hips
-                x_pos = left_shoulder[0]
-                y_pos = left_shoulder[0]
+        if right_hip:
+            right_hip_x, right_hip_y = right_hip[0], right_hip[1]
+            Helper.put_text_on_frame(
+                frame,
+                f"Right Hip: ({right_hip_x}, {right_hip_y})",
+                (right_hip_x, right_hip_y - 20),
+            )
 
-                # Ensure the resized T-shirt fits within the frame
-                if y_pos + height > frame.shape[0]:
-                    y_pos = frame.shape[0] - height
+        if selected_t_shirt is not None:
+            shirt_height, shirt_width = selected_t_shirt.shape[:2]
 
-                if x_pos + width > frame.shape[1]:
-                    x_pos = frame.shape[1] - width
+            if left_shoulder and right_shoulder and left_hip and right_hip:
+                shoulder_distance = abs(left_shoulder[0] - right_shoulder[0])
+                hip_distance = abs(left_hip[0] - right_hip[0])
+                shoulder_hip_distance = abs(left_shoulder[1] - left_hip[1])
 
-                    # Overlay the T-shirt on the frame
-                frame[y_pos:y_pos + height, x_pos:x_pos + width] = resized_t_shirt
-        else:
-            # Center horizontally
-            x_pos = (frame_shape[1] - shirt_width) // 2
-            y_pos = 50  # Position vertically, adjust as needed
-            frame[y_pos:y_pos + shirt_height, x_pos:x_pos + shirt_width] = selected_t_shirt
+                # The width of the shirt should cover the greater distance between the shoulders or the hips
+                width_of_shirt = max(shoulder_distance, hip_distance)
 
-    cv2.imshow(APP_NAME, frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        run = False
-        break
+                #  The height of the shirt should cover the distance from shoulder to hip
+                height_of_shirt = shoulder_hip_distance
 
-cap.release()
-cv2.destroyAllWindows()
+                # Resize the T-shirt image to fit the calculated width and height
+                resized_t_shirt = cv2.resize(
+                    selected_t_shirt,
+                    (width_of_shirt, height_of_shirt),
+                )
+
+                try:
+                    # frame = Helper.overlay_image(frame, resized_t_shirt, right_shoulder[:2])
+                    frame = Helper.overlay_image(
+                        frame, resized_t_shirt,
+                        (right_shoulder[0], right_shoulder[1]),
+                    )
+                except Exception as e:
+                    logger.error(f"Error; {e}")
+                    pass
+
+        cv2.imshow(APP_NAME, frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            logger.info("Pressed q, closing window...")
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()
--- utils.py
+++ utils.py
@@ -1,4 +1,7 @@
+from typing import List, Tuple
+
 import cv2
+import numpy as np
 
 from logger import setup_logger
 
@@ -10,3 +13,56 @@ class Helper:
     @staticmethod
     def put_text_on_frame(frame, text, position, font=cv2.FONT_HERSHEY_PLAIN, scale=1, color=(0, 255, 0), thickness=2):
         cv2.putText(frame, text, position, font, scale, color, thickness)
+
+    @staticmethod
+    def overlay_image(img_back: np.ndarray, img_front: np.ndarray, pos: Tuple[int, int] = (0, 0)) -> np.ndarray:
+        """
+        Overlays a PNG image with transparency onto another image using alpha blending.
+
+        The function handles out-of-bound positions, including negative coordinates, by cropping
+        the overlay image accordingly. Edges are smoothed using alpha blending based on the alpha channel.
+
+        Args:
+            img_back (np.ndarray): The background image, expected to be of shape (height, width, 3) or (height, width, 4).
+                                   The background can have an optional alpha channel, but only the RGB channels will be used.
+            img_front (np.ndarray): The foreground PNG image to overlay, of shape (height, width, 4). The 4th channel
+                                    should be the alpha channel representing transparency.
+            pos (Tuple[int]): A list specifying the [x, y] coordinates (in pixels) at which to overlay the image.
+                             Can be negative or cause the overlay image to go out-of-bounds.
+
+        Returns:
+            np.ndarray: A new image with the overlay applied. The shape will be the same as `img_back`.
+        """
+
+        hf, wf, _ = img_front.shape
+        hb, wb, _ = img_back.shape
+
+        x1, y1 = max(pos[0], 0), max(pos[1], 0)
+        x2, y2 = min(pos[0] + wf, wb), min(pos[1] + hf, hb)
+
+        # Calculate overlay start coordinates for the overlay image
+        x1_overlay = max(0, -pos[0])
+        y1_overlay = max(0, -pos[1])
+
+        # Calculate the dimensions of the slice to overlay
+        wf, hf = x2 - x1, y2 - y1
+
+        # If overlay is completely outside background, return original background
+        if wf <= 0 or hf <= 0:
+            return img_back
+
+        # Extract the alpha channel from the foreground and create the inverse mask
+        alpha = img_front[y1_overlay:y1_overlay + hf, x1_overlay:x1_overlay + wf, 3] / 255.0
+        inv_alpha = 1.0 - alpha
+
+        # Extract the RGB channels from the foreground
+        img_rgb = img_front[y1_overlay:y1_overlay + hf, x1_overlay:x1_overlay + wf, 0:3]
+
+        # # Alpha blend the foreground and background
+        # for c in range(0, 3):
+        #     img_back[y1:y2, x1:x2, c] = img_back[y1:y2, x1:x2, c] * inv_alpha + img_rgb[:, :, c] * alpha
+
+        # Perform alpha blending using vectorized operations
+        img_back[y1:y2, x1:x2, :3] = (inv_alpha[..., None] * img_back[y1:y2, x1:x2, :3] + alpha[..., None] * img_rgb)
+
+        return img_back
